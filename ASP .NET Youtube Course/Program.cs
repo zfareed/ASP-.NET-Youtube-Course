9af987bc6d1989b0eae7224b4a185c676468c7d5
Zain Fareed <zainfareed093@gmail.com>
2020-05-19 13:59:36 +0500

    Created New Project

--- ASP .NET Youtube Course/Program.cs
+++ ASP .NET Youtube Course/Program.cs
@@ -6,7 +6,7 @@ namespace ASP_.NET_Youtube_Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(" Created new Project");
         }
     }
 }
